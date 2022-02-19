d59aab09e3da8ff94a2a43acbd33d0cebdc06859
rafaariza <silex.designs12@gmail.com>
2022-02-19 19:18:51 +0100

    Tarea de la primera semana

---
+++ scripts/semana1.py
@@ -0,0 +1,34 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy import integrate
+plt.rcParams.update({
+  "text.usetex": True,
+  "font.family": "Palatino"
+})
+
+i = 500
+k_interval = np.linspace(-i,i,2**14+1)
+
+def gaussian_function(k,sigma):
+    """
+    FALTA DOCSTRING
+    """
+    return np.exp(-k**2/(2*sigma**2))/(sigma*np.sqrt(2*np.pi))
+
+integral = integrate.quad(gaussian_function,-np.inf,np.inf,args=(20,))[0]
+
+fvalues = gaussian_function(k_interval,20)
+fft = np.fft.fftshift(np.fft.fft(fvalues))
+fft_norm = fft/max(fft)
+
+print(f"El área debajo de la gaussiana es {integral:1.0f}")
+
+plt.subplot(1,2,1)
+plt.plot(k_interval,fvalues,'r')
+plt.title(r"\(f(k)=\frac{1}{\sigma\sqrt{2\pi}}\ \mathrm{e}^{-\frac{k^2}{2\sigma^2}}\) con \(\sigma=20\)",fontsize=18)
+plt.subplot(1,2,2)
+plt.plot(k_interval,np.abs(fft_norm),'g')
+plt.title(r"DFT de \(f(k)\) mediante método FFT",fontsize=18)
+plt.xlim(-10,10)
+
+plt.show()
