b7c9b507c3dfd4841a74d724827f4882969250af
rafaariza <silex.designs12@gmail.com>
2022-03-02 23:50:10 +0100

    Updated for tuthors' meet

--- scripts/semana2.py
+++ scripts/semana2.py
@@ -4,7 +4,7 @@ DOCSTRING
 import numpy as np
 import matplotlib.pyplot as plt
 
-m=2**10
+m = 2**11
 k = np.arange(-m,m+1,dtype=float)
 
 np.random.seed(32)
@@ -12,15 +12,15 @@ values = list(np.random.normal(size=m)+1j*np.random.normal(size=m))
 valuesc = [np.conj(i) for i in values][::-1]
 center = [np.random.normal()]
 
-v = np.array(valuesc+center+values)
+v = np.array(center+values+valuesc)
 
-amplitude = np.sqrt(k**2)
+amplitude = k**2
 
-gfield = np.fft.ifft(v * amplitude)
+gfield = np.fft.ifftshift(v)
 gfield = gfield - np.mean(gfield)
 gfield = gfield/np.std(gfield)
 
-gfield2 = np.fft.fft(v * amplitude)
+gfield2 = np.fft.fftshift(v)
 gfield2 = gfield2 - np.mean(gfield2)
 gfield2 = gfield2/np.std(gfield2)
 
