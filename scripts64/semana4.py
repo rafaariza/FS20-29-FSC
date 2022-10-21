6368a02881b2a81cc30c6289e209526eb3679fd6
rafaariza <silex.designs12@gmail.com>
2022-10-21 05:44:00 +0200

    Latest 21/10

--- scripts64/semana4.py
+++ scripts64/semana4.py
@@ -1,7 +1,6 @@
 """
-DOCSTRING
+Módulo Python del TFG Generación de campos gaussianos en teorías inflacionarias.
 """
-import time
 import numpy as np
 import matplotlib.pyplot as plt
 plt.rcParams.update({
@@ -11,9 +10,12 @@ plt.rcParams.update({
         'xtick.direction': 'in',
         'xtick.top': True,
         'ytick.direction' : 'in',
-        'text.usetex': True,
-        'ytick.right': True})
+        'ytick.right': True,
+        'text.usetex': True})
 from matplotlib.colors import ListedColormap
+from powerbox.tools import _magnitude_grid as mgrid
+from powerbox.powerbox import _make_hermitian as hermitian
+from powerbox.dft import fftfreq as _fftfreq, ifft as _ifftn
 
 timestr = "DEF"
 k0 = 0.05
@@ -22,25 +24,36 @@ class FS2029FSC():
     """
     DOCSTRING
     """
-    def __init__(self, amplitude, power, size, dimensions, show, method):
+    def __init__(self, amplitude, power, size, dimensions, show, method,
+                boxlength = 1.0, normalise_power = True, a = 1., b = 1.):
+
         self._amplitude = amplitude
+        self._boxlength = boxlength
+        self._normalised_power = normalise_power
+        self._coef_a = a
+        self._coef_b = b
         self._power = int(power)
         self._size = int(np.power(2, size))
         self._dimensions = dimensions
         self._show = show
         self._method = method
+        self._volume = self._boxlength ** self._dimensions
+        self._dx = float(boxlength) / self._size
+        self.n = self._size + 1
 
     def generator(self):
         """
         DOCSTRING
         """
-        np.random.seed(2029)
-        seed_gaussiano = np.random.normal(size=[self._size] * self._dimensions)
-        seed_gaussiano_fourier = np.fft.fftn(seed_gaussiano)
+        rng = np.random.default_rng(seed=42)
+        seed_gaussiano_mag = rng.normal(0.0, 1.0, size = [self.n] * self._dimensions)
+        seed_gaussiano_phase = 2 * np.pi * rng.uniform(size = [self.n] * self._dimensions)
+        seed_gaussiano_hermitian = hermitian(seed_gaussiano_mag, seed_gaussiano_phase)
+        cutindex = (slice(None, -1),) * self._dimensions
+        seed_gaussiano_hermitian = seed_gaussiano_hermitian[cutindex]
 
-        vectork = np.fft.fftfreq(self._size) * self._size
-        kx, ky, kz = np.meshgrid(vectork, vectork, vectork, indexing='xy')
-        normks = np.sqrt(np.power(kx,2) + np.power(ky,2) + np.power(kz,2))
+        vectork = _fftfreq(self._size, d = self._dx, b = self._coef_b)
+        normks = mgrid(vectork, self._dimensions)
         if self._power < 0:
             normks[0,0,0] = 10E20
         else:
@@ -48,9 +61,16 @@ class FS2029FSC():
         self._normks = normks
 
         p_spectro, self._name = self._method(self._amplitude, self._normks, self._power)
+
+        if self._normalised_power:
+            p_spectro[...] = p_spectro / self._volume
+ 
         p_spectro_root = np.sqrt(p_spectro)
-        self._k_realize = seed_gaussiano_fourier * p_spectro_root
-        self._x_realize = np.fft.fftn(self._k_realize).real
+        self._k_realize = seed_gaussiano_hermitian * p_spectro_root
+        self._x_realize = np.empty((self._size,) * self._dimensions, dtype = 'complex128')
+        self._x_realize[...] = self._k_realize
+        self._x_realize[...] = self._volume * _ifftn(self._x_realize, L = self._boxlength, a = self._coef_a, b = self._coef_b)[0]
+        self._x_realize = np.real(self._x_realize)
         np.clip(self._x_realize, -1, np.inf, self._x_realize)
 
         return self._x_realize
@@ -70,10 +90,9 @@ class FS2029FSC():
         for ind, realization in enumerate(self._x_realize[::len(self._x_realize)]):
             fig, ax = plt.subplots()
             fig.set_size_inches(w=8,h=4.94)
-            plot = ax.imshow(realization, cmap=cmap)
-            #fig.suptitle(f'Realización campo gaussiano espacio real XY con potencia {self._power}. {self._name}')
+            plot = ax.imshow(realization, cmap="blue")
             cbar = fig.colorbar(plot)
-            cbar.set_ticks([])
+            #cbar.set_ticks([])
             cbar.set_label(r'$\delta$')
             ax.set_xlabel(r'$x$')
             ax.set_ylabel(r'$y$')
@@ -93,28 +112,16 @@ def transferfunction_k(amplitude, modulus, power):
     C0 = 14.2 + 731 * np.power(1 + 62.5 * q, -1)
     T0 = L0 * np.power(L0 + C0 * np.power(q, 2), -1)
     T0[0,0,0] = 0
-    name = "TF_method"
+    name = "transfer"
     return amplitude * np.power(q, 1 * power) * np.power(T0, 2), name
 
 def k_pol(amplitude, modulus, power):
     """
     DOCSTRING
     """
-    name = "Only_polynomial_of_k"
+    name = "pdek"
     return amplitude * np.power(modulus/k0, 1 * power), name
 
-
-#for power in range(-4,2):
-#    TransFunc = FS2029FSC(
-#        amplitude=2E-9, size=9, power=power, dimensions=3, show=False, method=transferfunction_k)
-#    TransFunc.generator()
-#    TransFunc.show_x_realize()
-
-#for power in range(-4,2):
-#    polynomial = FS2029FSC(amplitude=2E-9, size=9, power=power, dimensions=3, show=False, method=k_pol)
-#    polynomial.generator()
-#    polynomial.show_x_realize()
-
-testing = FS2029FSC(amplitude = 2.101E-9, power = -3, size = 9, dimensions = 3, show = True, method = k_pol)
+testing =  FS2029FSC(amplitude=2.101E-9, power=5, size=9, dimensions=3, show=True, method=transferfunction_k, boxlength=5.0)
 testing.generator()
 testing.show_x_realize()
