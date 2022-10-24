a95ca5ba29c9f48733335eed19b9521941b35648
rafaariza <silex.designs12@gmail.com>
2022-10-24 21:44:14 +0200

    Terminado lo grande

--- scripts64/semana4.py
+++ scripts64/semana4.py
@@ -10,9 +10,9 @@ plt.rcParams.update({
         'xtick.direction': 'in',
         'xtick.top': True,
         'ytick.direction' : 'in',
-        'ytick.right': True})
-#        'text.usetex': True})
-from matplotlib.colors import ListedColormap
+        'ytick.right': True,
+        'text.usetex': True})
+from matplotlib.colors import ListedColormap, CenteredNorm
 from powerbox.tools import _magnitude_grid as _mgrid
 from powerbox.powerbox import _make_hermitian as _hermitian
 from powerbox.dft import fftfreq as _fftfreq, ifft as _ifftn
@@ -22,7 +22,7 @@ k0 = 0.05
 
 class FS2029FSC():
     """
-    DOCSTRING
+    Calcula campos gaussianos en espacio real y de fourier generados con un espectro de potencia dado.
     """
     def __init__(self, amplitude, power, size, dimensions, show, method,
                 boxlength=1.0, normalise=True, a=1., b=1.):
@@ -43,7 +43,7 @@ class FS2029FSC():
 
     def generator(self):
         """
-        DOCSTRING
+        Genera el campo de fluctuaciones de densidad de energía δ(x).
         """
         rng = np.random.default_rng(seed=42)
         seed_gaussiano_mag = rng.normal(0.0,1.0, size=[self._n] * self._dimensions)
@@ -52,19 +52,17 @@ class FS2029FSC():
         cutindex = (slice(None, -1),) * self._dimensions
         seed_gaussiano_hermitian = seed_gaussiano_hermitian[cutindex]
 
-        vectork = _fftfreq(self._size, d=self._dx, b=self._coef_b)
-        normks = _mgrid(vectork, self._dimensions)
-        if self._power < 0:
-            normks[0,0,0] = 10E20
-        else:
-            normks[0,0,0] = 0
+        self._vectork = _fftfreq(self._size, d=self._dx, b=self._coef_b)
+        normks = _mgrid(self._vectork, self._dimensions)
+        mask = normks != 0
         self._normks = normks
-        print(self._normks.shape)
-        p_spectro, self._name = self._method(self._amplitude, self._normks, self._power)
+        self._normks[mask], self._name = self._method(self._amplitude, self._normks[mask], self._power)
 
         if self._normalise:
-            p_spectro[...] = p_spectro / self._volume
- 
+            p_spectro = self._normks / self._volume
+        else:
+            p_spectro = self._normks
+
         p_spectro_root = np.sqrt(p_spectro)
         self._k_realize = seed_gaussiano_hermitian * p_spectro_root
         self._x_realize = np.empty((self._size,) * self._dimensions,
@@ -76,10 +74,10 @@ class FS2029FSC():
         np.clip(self._x_realize, -1, np.inf, self._x_realize)
 
         return self._x_realize
-        
+
     def show_x_realize(self):
         """
-        Realización en el espacio de las posiciones
+        Realización en el espacio de las posiciones.
         """
         if not hasattr(self, '_x_realize'):
             print('Genera primero el campo con generator()')
@@ -89,41 +87,42 @@ class FS2029FSC():
         planck_cmap.set_bad("gray")
         planck_cmap.set_under("white")
         cmap = planck_cmap
-        for ind, realization in enumerate(self._x_realize[::len(self._x_realize)]):
-            fig, ax = plt.subplots()
-            fig.set_size_inches(w=8, h=4.94)
-            plot = ax.imshow(realization, cmap=cmap)
-            cbar = fig.colorbar(plot)
-            #cbar.set_ticks([])
-            cbar.set_label(r'$\delta$')
-            ax.set_xlabel(r'$x$')
-            ax.set_ylabel(r'$y$')
+        for ind, realization in enumerate(self._x_realize[::10]):
+            fig, ax = plt.subplots(figsize=(5,5))
+            fig.subplots_adjust(top = 1, bottom = 0.1, right = 1, left = 0)
+            ax.set_box_aspect(1)
+            plot = ax.imshow(realization, norm=CenteredNorm(), cmap=cmap, extent=(0,self._boxlength*self._size,0,self._boxlength*self._size))
+            cbar = fig.colorbar(plot, orientation='horizontal', fraction=0.042, pad=0.1)
+            cbar.set_label(r'$\delta(\mathbf{x})$', rotation=0, size=12)
+            ax.set_xlabel(r'$x$ (Mpc)')
+            ax.set_ylabel(r'$y$ (Mpc)')
             if self._show is True:
                 plt.show()
             else:
                 plt.savefig(
                     f"R:\\Documentos\\FS20-29-FSC\\scripts64\\img\\{self._name}-({self._power})-{timestr}-{ind}.pgf",
-                        dpi=300, bbox_inches='tight')
+                        dpi=600, bbox_inches='tight', pad_inches=0.0)
+            plt.close(fig)
 
 def transferfunction_k(amplitude, modulus, power):
     """
-    DOCSTRING
+    Espectro de potencias lineal.
     """
     q = modulus/(0.14*k0)
     L0 = np.log(2 * np.exp(1) + 1.8 * q)
     C0 = 14.2 + 731 * np.power(1 + 62.5 * q, -1)
     T0 = L0 * np.power(L0 + C0 * np.power(q, 2), -1)
-    T0[0,0,0] = 0
     name = "transfer"
     return amplitude * np.power(q, 1 * power) * np.power(T0, 2), name
 
+
 def k_pol(amplitude, modulus, power):
     """
-    DOCSTRING
+    Espectro de potencias primordial.
     """
     name = "pdek"
-    return amplitude * np.power(modulus/k0, 1 * power), name
+    return amplitude * (modulus/k0) ** power, name
 
-testing =  FS2029FSC(amplitude=2.101E-9, power=1, size=8, dimensions=3, show=True, method=transferfunction_k, boxlength=1.0)
-testing.generator()
-#testing.show_x_realize()
+delta_x = FS2029FSC(amplitude=2.101E-9, power=0.965, size=9, dimensions=3, show=False, method=transferfunction_k, boxlength=0.25)
+delta_x.generator()
+delta_x.show_x_realize()
